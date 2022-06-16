40550d95fd2b8b6ed8fa3892ce39f60b3baedef8
Leonardo Santos <81315155+Je-UFPR@users.noreply.github.com>
2022-06-16 11:49:00 -0300

    teste

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include "LiquidCrystal.h" 
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(3, 0);
+  lcd.print("TESTE LCD");
+  lcd.setCursor(3, 1);
+  lcd.print("Teste");
+  delay(3000);
+
+  for (int poss = 0; poss < 3; poss++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(100);
+  }
+
+  for (int poss = 0; poss < 6; poss++)
+  {
+    lcd.scrollDisplayRight();
+    delay(100);
+  }
+  for (int poss = 0; poss < 3; poss++)
+  {
+    lcd.scrollDisplayLeft();
+    delay(100);
+  }
+}
